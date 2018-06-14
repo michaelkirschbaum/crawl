dd499da5c7402b5fb490f43dbc1a95b3e50b2922
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-14 09:26:09 -0700

    add App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, createContext } from 'react';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Crawl</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App.js";
 ReactDOM.render(
-  <h1>Crawl</h1>,
+  <App />,
   document.getElementById('root')
 );
