0f2764f61c236d5a35d7ef38fd7609d18aa8efa9
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-19 09:32:08 -0700

    create redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/App/App.js";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from "./components/App/App.js"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+// import rootReducer from './reducers'
+
+const store = createStore(function app(state = [], action) {
+  return state
+})
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
