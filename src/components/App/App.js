0beb218573856aab0b49c7e4321953d9a93ba7a2
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-19 08:40:44 -0700

    add Menu.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component, createContext } from 'react'
 import "./App.css"
 import { hot } from 'react-hot-loader'
+import Menu from "../Menu/Menu.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Menu />
       </div>
     );
   }
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./Menu.css"
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+      </div>
+    );
+  }
+}
+
+export default Menu
