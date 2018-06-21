c43a57dc3eb9370c0201673cae7ee2503ec0ee1c
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-21 13:53:39 -0700

    use mockups api

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
+var mockups = require('./api/mockups')
+
 var express = require('express')
 var app = express()
 
 const PORT = 8080
 
 app.get('/', function(req, res) {
-  res.send('crawlr')
-})
-
-app.get('/user', function(req, res) {
   res.send('')
 })
+
+app.use('/mockups', mockups)
 
 app.listen(PORT, () => console.log("listening on port 8080"))
