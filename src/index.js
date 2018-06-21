948980c1a232efb387c0915266bb65ff39e60374
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-21 10:21:33 -0700

    configure devtools

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-const store = createStore(function app(state = [], action) {
-  return state
-})
+const store = createStore(
+  function app(state = [], action) {
+    return state
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
