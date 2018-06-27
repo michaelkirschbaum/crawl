186d7cf7f5818d6689e98ed3139ee9271027c398
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-27 12:38:38 -0700

    add Upload.js

---
+++ web/components/upload/Upload.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./Upload.css"
+
+class Upload extends Component {
+  render() {
+    return (
+      <div className="Upload">
+        <h1>Upload</h1>
+      </div>
+    )
+  }
+}
+
+export default Upload
