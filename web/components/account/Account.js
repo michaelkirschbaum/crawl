0d5f9b3625bbbdee9564c23c36df0aabae00ff1b
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-27 14:05:11 -0700

    move Upload to dashboard

--- web/components/account/Account.js
+++ web/components/account/Account.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import "./Account.css"
-import Upload from "../upload/Upload.js"
 
 class Account extends Component {
   render() {
     return (
       <div className="Account">
-        <Upload />
       </div>
     );
   }
--- web/components/dashboard/Dashboard.js
+++ web/components/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Upload from "../upload/Upload.js"
 import "./Dashboard.css"
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="Dashboard">
+        <Upload />
       </div>
     );
   }
