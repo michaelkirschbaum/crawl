761f4dd0c9c0f610ae001392373954dcf9711bdd
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-27 15:25:33 -0700

    handle input submit

--- web/components/upload/Upload.js
+++ web/components/upload/Upload.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import "./Upload.css"
 
 class Upload extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {image: null}
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(event) {
+    alert('Upload was successful')
+    event.preventDefault()
+  }
+
   render() {
     return (
       <div className="Upload">
-        <form>
-          Design:
+        <form onSubmit={this.handleSubmit}>
           <input type="file" name="design"/>
           <input type="submit" value="Submit" />
         </form>
