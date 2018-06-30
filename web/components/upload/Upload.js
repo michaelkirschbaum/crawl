bda5f15549956ead6a1882ae3b1fb1e6ca2ddbb3
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-29 17:12:27 -0700

    implement handleSubmit

--- web/components/upload/Upload.js
+++ web/components/upload/Upload.js
@@ -6,17 +6,22 @@ class Upload extends Component {
     super(props)
     this.state = {file: null}
     this.onChange = this.onChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   onChange(event) {
     this.setState({file: URL.createObjectURL(event.target.files[0])})
   }
 
+  handleSubmit(event) {
+    alert("project submitted")
+  }
+
   render() {
     return (
       <div className="Upload">
         <h1>Projects</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           Name: <input type="text" name="name" /><br />
           <input type="file" accept="image/png, image/jpeg" name="mockup" onChange={this.onChange} /><br />
           <input type="submit" value="Submit" />
