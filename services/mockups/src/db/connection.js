a4b3df4b60db9a76417b830e49508b4c2ae7b402
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-06-30 18:32:07 -0700

    create test mockup object

--- services/mockups/src/db/connection.js
+++ services/mockups/src/db/connection.js
@@ -1,10 +1,19 @@
 var mongoose = require('mongoose')
 
 mongoose.connect('mongodb://mockups-db/test')
+
 var db = mongoose.connection
-var schema = mongoose.Schema
+
+var Schema = mongoose.Schema
+var mockupsSchema = new Schema({
+  name: String
+})
 
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log('database connected')
 })
+
+var Mockup = mongoose.model('mockupModel', mockupsSchema)
+
+module.exports = Mockup
