3ed4a2f46d8f3bfeafbbf5c390876a74ca42c1d8
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-07-03 13:37:01 -0700

    set upload key

--- services/mockups/src/db/queries.js
+++ services/mockups/src/db/queries.js
@@ -1,6 +1,7 @@
 var Mockup = require('./connection')
 var AWS = require('aws-sdk')
 var uuid = require('uuid')
+var path = require('path')
 
 AWS.config.update({
   region: 'us-west-2'
@@ -12,12 +13,33 @@ var uri = null
 
 s3.createBucket({Bucket: bucketName}, function(err, data) {
   if (err) console.log(err, err.stack)
-  else uri = data.Location
+  else console.log("created new s3 bucket", data.Location)
 })
 
 function addMockup(req, res, next) {
-  const mockup = new Mockup({ name: req.body.name, uri: uri })
+  var uploadParams = {
+    Bucket: bucketName,
+    Key: path.basename(req.body.uri),
+    Body: ''
+  }
 
+/*
+  var fs = require('fs');
+  var fileStream = fs.createReadStream(file);
+  fileStream.on('error', function(err) {
+    console.log('File Error', err);
+  });
+  uploadParams.Body = fileStream;
+*/
+
+/*
+  s3.upload(uploadParamas, function(err, data) {
+    if (err) console.log("error uploading to s3", err)
+    else console.log("upload successful", data.Location)
+  })
+*/
+  const mockup = new Mockup({ name: req.body.name, uri: req.body.uri })
+  console.log(uploadParams.Key)
   mockup.save()
     .then(() => res.send('added mockup'))
 }
