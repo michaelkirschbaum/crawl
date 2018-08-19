0a4f390358b6e4c59529c73615aec8861dbf0d9a
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-19 14:58:42 -0700

    call putBucketCors in createBucket scope

--- services/mockups/src/db/queries.js
+++ services/mockups/src/db/queries.js
@@ -9,15 +9,6 @@ AWS.config.update({
 
 var s3 = new AWS.S3()
 var bucketName = 'crawlr' + uuid.v4()
-
-s3.createBucket({Bucket: bucketName}, function(err, data) {
-  if (err)
-    console.log(err, err.stack)
-  else
-    console.log("created new s3 bucket", data.Location)
-})
-
-// set cors for bucket
 var corsConfig = {
   AllowedHeaders:["Authorization"],
   AllowedMethods:["PUT", "POST", "DELETE"],
@@ -30,11 +21,21 @@ var corsParams = {
   Bucket: bucketName,
   CORSConfiguration: {CORSRules: new Array(corsConfig)}
 }
-s3.putBucketCors(corsParams, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data);
+
+s3.createBucket({Bucket: bucketName}, function(err, data) {
+  if (err)
+    console.log(err, err.stack)
+  else {
+    console.log("created new s3 bucket", data.Location)
+
+    // set bucket cors
+    s3.putBucketCors(corsParams, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    })
   }
 })
 
