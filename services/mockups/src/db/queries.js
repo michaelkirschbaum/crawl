2f1a3d1819dc2b5179b66a422c406f13ae5c92c7
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-19 14:42:28 -0700

    merge 'projects' fixes #3

--- services/mockups/src/db/queries.js
+++ services/mockups/src/db/queries.js
@@ -17,6 +17,27 @@ s3.createBucket({Bucket: bucketName}, function(err, data) {
     console.log("created new s3 bucket", data.Location)
 })
 
+// set cors for bucket
+var corsConfig = {
+  AllowedHeaders:["Authorization"],
+  AllowedMethods:["PUT", "POST", "DELETE"],
+  AllowedOrigins:["*"],
+  ExposeHeaders:[],
+  MaxAgeSeconds:3000
+}
+
+var corsParams = {
+  Bucket: bucketName,
+  CORSConfiguration: {CORSRules: new Array(corsConfig)}
+}
+s3.putBucketCors(corsParams, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+})
+
 function signUrl(req, res, next) {
   const { fileName, fileType } = req.query
 
