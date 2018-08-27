4c43886bb52523f49ba82a8656d401c87b08337d
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-27 00:05:56 -0700

    create shallow snapshot

--- web/components/__tests__/Upload.test.js
+++ web/components/__tests__/Upload.test.js
@@ -4,17 +4,14 @@ import Upload from '../../components/upload/Upload'
 import fetchMock from 'fetch-mock'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import renderer from 'react-test-renderer'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('Upload', () => {
-  fetchMock.get('*', JSON.stringify({response: ''}))
+  fetchMock.get('*', JSON.stringify({res: ''}))
 
   it('should render correctly', () => {
-    const component = renderer.create(
-      <Upload />
-    ).toJSON()
+    const component = shallow(<Upload />)
     expect(component).toMatchSnapshot()
   })
 
