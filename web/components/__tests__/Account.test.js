747b6958ee83ebb3bc847e9d16415cb3fcd9632f
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-27 00:12:01 -0700

    add test case

--- web/components/__tests__/Account.test.js
+++ web/components/__tests__/Account.test.js
@@ -1,7 +1,14 @@
+import React from 'react'
 import Account from '../../components/account/Account'
+import { shallow } from 'enzyme'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 describe('Account', () => {
-  it('should', () => {
-
+  it('should render correctly', () => {
+    const component = shallow(<Account />)
+    expect(component).toMatchSnapshot()
   })
 })
