1089f46c4f742d8a1f3e9862fc86a65678431f15
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-26 21:29:26 -0700

    configure enzyme

--- web/components/__tests__/Upload.test.js
+++ web/components/__tests__/Upload.test.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 import Upload from '../../components/upload/Upload'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import fetchMock from 'fetch-mock'
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Upload', () => {
-  it('should', () => {
-    const component = renderer.create(
+  it('should render correctly', () => {
+    /* const component = shallow(
+      <Upload />
     )
+    expect(component).toMatchSnapshot() */
+  })
+
+  it('should fetch projects', () => {
+    fetchMock.get('*', JSON.stringify({response: ''}))
+
+    // expect.assertion(1)
+    // return getImage('test.jpeg').then
+  })
+
+  it('should set error when getImage fails', () => {
+
+  })
+
+  describe('', () => {
+    it('', () => {
+
+    })
   })
 })
