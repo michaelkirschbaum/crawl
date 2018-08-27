9aac98700eab42ab5bf62c2b51780a1c9ce3c407
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-27 01:27:29 -0700

    make test cases fail

--- web/components/__tests__/Upload.test.js
+++ web/components/__tests__/Upload.test.js
@@ -16,36 +16,36 @@ describe('Upload', () => {
   })
 
   it('should lookup projects', () => {
-
+    expect(false).toBe(true)
   })
 
   it('should show error when project lookup fails', () => {
-
+    expect(false).toBe(true)
   })
 
   it('should show spinner when loading project', () => {
-
+    expect(false).toBe(true)
   })
 
   it('set error when loading project fails', () => {
-
+    expect(false).toBe(true)
   })
 
   describe('submit is clicked', () => {
     it('should save project', () => {
-
+      expect(false).toBe(true)
     })
 
     it('should set error when unable to save project', () => {
-
+      expect(false).toBe(true)
     })
 
     it('should upload image', () => {
-
+      expect(false).toBe(true)
     })
 
     it('should set project in state', () => {
-      
+      expect(false).toBe(true)
     })
   })
 })
