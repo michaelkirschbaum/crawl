8e8c31b4747d6da2c02909f8e09bb4fbbc03d1eb
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-27 02:37:59 -0700

    create aws.js

--- services/mockups/src/db/queries.js
+++ services/mockups/src/db/queries.js
@@ -1,49 +1,11 @@
 var Mockup = require('./connection')
-var AWS = require('aws-sdk')
-var uuid = require('uuid')
-
-// initiate s3
-AWS.config.update({
-  region: 'us-west-2'
-})
-
-var s3 = new AWS.S3()
-var bucketName = 'crawlr' + uuid.v4()
-var corsConfig = {
-  AllowedHeaders:["*"],
-  AllowedMethods:["PUT", "POST", "DELETE"],
-  AllowedOrigins:["*"],
-  ExposeHeaders:[],
-  MaxAgeSeconds:3000
-}
-
-var corsParams = {
-  Bucket: bucketName,
-  CORSConfiguration: {CORSRules: new Array(corsConfig)}
-}
-
-s3.createBucket({Bucket: bucketName}, function(err, data) {
-  if (err)
-    console.log(err, err.stack)
-  else {
-    console.log("created new s3 bucket", data.Location)
-
-    // set bucket cors
-    s3.putBucketCors(corsParams, function(err, data) {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log("Success", data);
-      }
-    })
-  }
-})
+var AWS = require('../../helpers/aws')
 
 function signUrl(req, res, next) {
   const { method, fileName, fileType } = req.query
 
   var s3Params = {
-    Bucket: bucketName,
+    Bucket: AWS.bucket,
     Key: fileName,
   }
 
@@ -51,13 +13,13 @@ function signUrl(req, res, next) {
   if (fileType)
     s3Params.ContentType = fileType
 
-  s3.getSignedUrl(method + 'Object', s3Params, (err, data) => {
+  AWS.s3.getSignedUrl(method + 'Object', s3Params, (err, data) => {
     if (err) {
       console.error(err)
     } else {
       res.json({
         signedRequest: data,
-        url: `https://${bucketName}.s3.amazonaws.com/${fileName}`
+        url: `https://${AWS.bucket}.s3.amazonaws.com/${fileName}`
       })
     }
   })
