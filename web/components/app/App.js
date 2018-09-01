ab858c5fefc770c8c5547e76e34c236db84c477f
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-08-31 21:38:00 -0700

    delete Main.js

--- web/components/app/App.js
+++ web/components/app/App.js
@@ -1,18 +1,24 @@
-import React, { Component, createContext } from 'react'
-import "./App.css"
+import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import Dashboard from "../dashboard/Dashboard.js"
+import Account from "../account/Account.js"
+import Login from "../login/Login"
 import Menu from "../menu/Menu"
-import Main from "../main/Main"
-import { Link, BrowserRouter as Router, Route } from 'react-router-dom'
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div>
         <Menu />
-        <Main />
+        <Switch>
+          <Route exact path='/' component={Dashboard}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/account' component={Account}/>
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
--- web/components/menu/Menu.js
+++ web/components/menu/Menu.js
@@ -7,6 +7,7 @@ class Menu extends Component {
     return (
       <div className="Menu">
         <Link to="/">Dashboard</Link>
+        <Link to="/login">Login</Link>
         <Link to="/account">Account</Link>
       </div>
     )
