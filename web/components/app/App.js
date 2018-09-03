3614052bca785dcbe47478bca36318a79870ead1
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-09-02 18:52:09 -0700

    add ProtectedRoute

--- web/components/app/App.js
+++ web/components/app/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import { connect } from 'react-redux'
-import { setAuthentication } from './appActions'
-import Dashboard from "../dashboard/Dashboard.js"
-import Account from "../account/Account.js"
-import Login from "../login/Login"
-import Menu from "../menu/Menu"
+import Dashboard from '../dashboard/Dashboard.js'
+import Account from '../account/Account.js'
+import Login from '../login/Login'
+import Menu from '../menu/Menu'
 import {
   Link,
   BrowserRouter as Router,
   Switch,
   Route,
-  withRouter
+  withRouter,
+  Redirect
 } from 'react-router-dom'
-import "./App.css"
+import './App.css'
 
 const mapStateToProps = state => ({
   isAuthenticated: state.isAuthenticated
 })
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setAuthentication: status => {
-      dispatch(setAuthentication(status))
-    }
-  }
-}
-
-const ProtectedRoute = () => (
+const ProtectedRoute = ({ component: Component }) => (
   <Route
-    render={<Component />}
+    render={props =>
+      false ? (
+        <Component />
+      ) : (
+        <Redirect to={{ pathname: '/login' }}/>
+      )}
   />
 )
 
@@ -41,11 +38,11 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Dashboard}/>
           <Route path='/login' component={Login}/>
-          <Route path='/account' component={Account}/>
+          <ProtectedRoute path='/account' auth={this.props.isAuthenticated} component={Account}/>
         </Switch>
       </div>
     )
   }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(hot(module)(App)))
+export default withRouter(connect(mapStateToProps)(hot(module)(App)))
