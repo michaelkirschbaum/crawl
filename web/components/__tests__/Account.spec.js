60552f55994095e81599f870f52d7dc502787991
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-09-02 23:18:18 -0700

    add Account.spec.js

---
+++ web/components/__tests__/Account.spec.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Account from '../../components/account/Account'
+import { shallow } from 'enzyme'
+
+describe('Account', () => {
+  it('should render correctly', () => {
+    const component = shallow(<Account />)
+    expect(component).toMatchSnapshot()
+  })
+})
