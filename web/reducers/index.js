b0e87c02bced1ab28eecbc6de9ac5bcd3a96b800
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-09-03 00:14:22 -0700

    add uploadReducer to root

--- web/reducers/index.js
+++ web/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import appReducer from './app'
+import uploadReducer from './upload'
 
 export default combineReducers({
-  appReducer
+  appReducer,
+  uploadReducer
 })
