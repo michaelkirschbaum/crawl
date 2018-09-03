7c28cec73c2ad07f743876a5afb32de9ace61ead
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-09-02 23:18:44 -0700

    add Login.spec.js

--- web/components/__tests__/Login.spec.js
+++ web/components/__tests__/Login.spec.js
@@ -1,2 +1,9 @@
 import React from 'react'
-import Upload from '../../components/login/Login'
+import Login from '../../components/login/Login'
+import { shallow } from 'enzyme'
+
+describe('Login', () => {
+  it('should render correctly', () => {
+    const component = shallow(<Login />)
+    expect(component).toMatchSnapshot()
+  })
