93f5884070b5289e373d854ac090256910b56e74
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-09-02 23:18:25 -0700

    add Register.spec.js

---
+++ web/components/__tests__/Register.spec.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Register from '../../components/register/Register'
+import { shallow } from 'enzyme'
+
+describe('Register', () => {
+  it('should render correctly', () => {
+    const component = shallow(<Register />)
+    expect(component).toMatchSnapshot()
+  })
