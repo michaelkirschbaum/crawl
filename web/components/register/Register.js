386b50899bffb2c3589a2814d34ea1970ee17e3f
Michael Kirschbaum <mkirschbaum915@gmail.com>
2018-09-02 22:42:14 -0700

    add Register

---
+++ web/components/register/Register.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Register.css'
+
+class Register extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="Register">
+        <h1>Register</h1>
+      </div>
+    )
+  }
+}
